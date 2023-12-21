8b240252a49c9afc5bc71b022af13f1cc65e953f
Sandeshbhat234 <101869011+Sandeshbhat234@users.noreply.github.com>
2023-12-21 15:36:36 +0530

    working on auth rule

--- src/Components/Photo.jsx
+++ src/Components/Photo.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
 import { useEffect, useState } from "react";
 import { generateClient } from "aws-amplify/api";
 import { createPhoto, deletePhoto } from "../graphql/mutations";
 import { listPhotos } from "../graphql/queries";
 import { Button } from "@aws-amplify/ui-react";
+
 const client = generateClient();
 
 const Photo = () => {
@@ -23,7 +23,7 @@ const Photo = () => {
       const photoData = result.data.listPhotos.items;
       setPhotos(photoData);
     } catch (error) {
-      console.log("error fetch", error);
+      console.log("error fetching text", error);
     }
   };
 
@@ -35,13 +35,14 @@ const Photo = () => {
           input: {
             name: name,
             description: description,
+            owner: "pamaso1948@anawalls.com"
           },
         },
       });
 
       console.log("photo uploaded", result);
     } catch (error) {
-      console.log("error", error);
+      console.log("error uploading", error);
     }
   };
 
@@ -49,8 +50,8 @@ const Photo = () => {
     event.preventDefault();
     loadPhotos();
     fetchPhotos();
-    setName('')
-    setDescription('')
+    setName("");
+    setDescription("");
   };
   const inputChangeHandler = (event) => {
     setName(event.target.value);
@@ -76,38 +77,78 @@ const Photo = () => {
   };
 
   return (
-    <>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="upload">
-          <h2>name</h2>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: "#f0f8ff",
+        padding: "20px",
+      }}>
+      <form
+        onSubmit={submitHandler}
+        style={{
+          marginBottom: "20px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}>
+        <label
+          htmlFor="upload"
+          style={{ color: "#ff4500", marginBottom: "10px" }}>
+          <h2>Name</h2>
         </label>
         <input
           id="name"
           type="text"
-          placeholder="type name"
+          placeholder="Type name"
           value={name}
           onChange={inputChangeHandler}
+          style={{ marginBottom: "10px" }}
         />
-        <label htmlFor="upload">
-          <h2>descprition</h2>
+        <label
+          htmlFor="upload"
+          style={{ color: "#ff4500", marginBottom: "10px" }}>
+          <h2>Description</h2>
         </label>
         <input
           id="description"
           type="text"
-          placeholder="describe"
+          placeholder="Describe"
           value={description}
           onChange={descriptionChangeHandler}
+          style={{ marginBottom: "10px" }}
         />
         <div>
-          <button>upload</button>
+          <button style={{ backgroundColor: "#32cd32", color: "white" }}>
+            Upload
+          </button>
         </div>
       </form>
-      <Button onClick={deletePhotos}>deleteee</Button>
-      <div>
-        <h2>uploaded data</h2>
+      <Button
+        onClick={deletePhotos}
+        style={{
+          backgroundColor: "#1e90ff",
+          color: "white",
+          marginBottom: "10px",
+        }}>
+        Delete
+      </Button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}>
+        <h2 style={{ color: "#ff4500" }}>State Data</h2>
         <div>
           {photos.map((item) => (
-            <div>
+            <div
+              style={{
+                backgroundColor: "#ffe4e1",
+                margin: "10px",
+                padding: "10px",
+              }}>
               <div key={item.id}>
                 {item.name} ---{item.description}
               </div>
@@ -115,8 +156,7 @@ const Photo = () => {
           ))}
         </div>
       </div>
-      ;
-    </>
+    </div>
   );
 };
 
--- src/Components/PhotoStorage.jsx
+++ src/Components/PhotoStorage.jsx
@@ -36,16 +36,16 @@ const PhotoStorage = () => {
   };
 
   return (
-    <div>
-      <form>
-        <label>photo</label>
-        <input type="text" onChange={(e) => setPhotoNameU(e.target.value)} />
-        <input type="file" onChange={(e) => setPhotoFile(e.target.files[0])} />
-        <Button onClick={storePhoto}>Upload</Button>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#f0f8ff', padding: '20px' }}>
+      <form style={{ marginBottom: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <label style={{ color: '#ff4500', marginBottom: '10px' }}>Photo</label>
+        <input type="text" onChange={(e) => setPhotoNameU(e.target.value)} style={{ marginBottom: '10px' }} />
+        <input type="file" onChange={(e) => setPhotoFile(e.target.files[0])} style={{ marginBottom: '10px' }} />
+        <Button onClick={storePhoto} style={{ backgroundColor: '#32cd32', color: 'white' }}>Upload</Button>
       </form>
-      <div>
-        <input type="text" onChange={(e) => setPhotoNameD(e.target.value)} />
-        <Button onClick={RestorePhoto}>Show Photo</Button>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <input type="text" onChange={(e) => setPhotoNameD(e.target.value)} style={{ marginBottom: '10px' }} />
+        <Button onClick={RestorePhoto} style={{ backgroundColor: '#1e90ff', color: 'white', marginBottom: '10px' }}>Show Photo</Button>
         <img src={photoUrl} alt="" width={500} />
       </div>
     </div>
